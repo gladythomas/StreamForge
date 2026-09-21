3bfa7f453f43e6b9c58fb48f7bed4abb65365cc1
gladythomas <gladychaprath1999@gmail.com>
2026-09-21 20:17:46 +0530

    feat: add Kafka consumer for video uploaded events

--- content-service/src/main/java/com/streamforge/service/VideoUploadedEncodedEventConsumer.java
+++ content-service/src/main/java/com/streamforge/service/VideoUploadedEncodedEventConsumer.java
@@ -1,5 +1,33 @@
 package com.streamforge.contentservice.service;
+package com.streamforge.contentservice.service;
 
+import com.streamforge.contentservice.model.VideoStatus;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class VideoUploadedEncodedEventConsumer {
+
+    private final ContentService contentService;
+
+    @KafkaListener(
+        topics = "video.uploaded"
+    )
+
+    public void consumerVideoUploadedEvent(
+        @Payload Map<String, Object> payload){
+    String movieId = (String) payload.get("movieId");
+    String videoKey= (String) payload.get("videoKey");
+
+    log.info("Video uploaded for movie: {} key: {}", movieId, videoKey);
+    contentService.updateVideoKey(movieId, videoKey);
+}
 
 }
